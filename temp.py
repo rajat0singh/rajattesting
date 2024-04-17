2bf5bb5eac5fc00e0715bc40393853568d9e5ef4
Rajat Singh <rajat.singh@corp.zscaler.com>
2024-04-17 12:38:32 +0530

    practise

--- temp.py
+++ temp.py
@@ -1,41 +1,111 @@
-# print("Hello world this is sample")
+# # print("Hello world this is sample")
 
 
-# count=5
-# print ("I need ", count)
+# # count=5
+# # print ("I need ", count)
 
 
-# numbers=["a","b","c","a"]
+# # numbers=["a","b","c","a"]
 
-# numbers.remove("a")
+# # numbers.remove("a")
 
 
-# numbers.sort(reverse=True)
+# # numbers.sort(reverse=True)
 
-# numbers.append("d")
-# numbers.append("k")
+# # numbers.append("d")
+# # numbers.append("k")
+
+# # print(numbers)
+
+
+# alpha = ['a', 'b', 'c', 'd', 'e']
+# numbers=alpha[0:5]
 
 # print(numbers)
+# print(alpha)
 
-
-alpha = ['a', 'b', 'c', 'd', 'e']
-numbers=alpha[0:5]
-
-print(numbers)
-print(alpha)
-
-nums=(1,2,3) 
+# nums=(1,2,3) 
 
 
 
-# () this is for tuples
-# {} this is for set
+# # () this is for tuples
+# # {} this is for set
 
 
-myset={'a','b','c','a','d'}
-print("hello my set")
-print(myset)
+# myset={'a','b','c','a','d'}
+# print("hello my set")
+# print(myset)
 
 
-for ele in myset:
-    print(ele)
+# for ele in myset:
+#     print(ele)
+
+
+# import os
+# import numpy as np
+
+# # Creating a 1D array
+# arr1 = np.array([1, 2, 3, 4, 5])
+# print("1D Array:")
+# print(arr1)
+
+
+# import calci
+
+# print(calci.add(123,45))
+
+# print(os.getcwd())
+
+# """
+# heloo rajat how are you 
+# """
+# print("something")
+# if 5 > 2:
+#   print("Five is greater than two!")
+
+
+# hey =1
+
+# print(type(hey))
+# xyz='a'
+# y=int(xyz)
+# print(y)
+
+# x="124"
+
+# print(type(int(x)))
+
+# x=y=1
+# print(x)
+# print(y)
+
+# x="hi"
+# y="bye"
+
+# print(x+" "+y)
+
+
+# def myfun():
+#     x=23
+#     print(x)
+
+
+# myfun()
+
+
+
+# x=12
+
+# def myfun():
+     
+#      global x
+#      x=13
+#      print(x)
+
+#      x=1400
+#      print(x)
+
+
+# myfun()
+
+# print(x)
