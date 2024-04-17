ba247333b3b4a8ec2f4b22dcddf50add75807e76
Rajat Singh <rajat.singh@corp.zscaler.com>
2024-04-17 10:59:36 +0530

    sample

---
+++ temp.py
@@ -0,0 +1,41 @@
+# print("Hello world this is sample")
+
+
+# count=5
+# print ("I need ", count)
+
+
+# numbers=["a","b","c","a"]
+
+# numbers.remove("a")
+
+
+# numbers.sort(reverse=True)
+
+# numbers.append("d")
+# numbers.append("k")
+
+# print(numbers)
+
+
+alpha = ['a', 'b', 'c', 'd', 'e']
+numbers=alpha[0:5]
+
+print(numbers)
+print(alpha)
+
+nums=(1,2,3) 
+
+
+
+# () this is for tuples
+# {} this is for set
+
+
+myset={'a','b','c','a','d'}
+print("hello my set")
+print(myset)
+
+
+for ele in myset:
+    print(ele)
